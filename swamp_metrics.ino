67821d303132efad4dc231c0561018634bec96c8
aidanvancil <42700427+aidanvancil@users.noreply.github.com>
2022-11-23 12:24:46 -0800

    Andrew + Aidan New Commits
    Added err messages, boolean constants, etc.

--- swamp_metrics.ino
+++ swamp_metrics.ino
@@ -1,15 +1,16 @@
 /*--------------------------------------------------
-Author:   Aidan Vancil + Andrew Gorum
-Project:  Creating A Embedded System (Swamp Cooler)
-Comments: N/A
-Date:     11/09/22
----------------------------------------------------*/
+  Author:   Aidan Vancil + Andrew Gorum
+  Project:  Creating A Embedded System (Swamp Cooler)
+  Commentars: N/A
+  Date:     11/09/22
+  ---------------------------------------------------*/
+// Internal Libraries
 #include <LiquidCrystal.h>
 #include <DHT.h>
+#include <Wire.h>
+//#include "DS1307.h"
 #include <DHT_U.h>
 #include <Stepper.h>
-//#include <Wire.h>
-//#include "DS1307.h"
 
 #define RDA 0x80
 #define TBE 0x20
@@ -19,19 +20,23 @@ float temp;
 #define DHTPIN 6       // DHT Pin
 #define DHTTYPE DHT11  // DHT Type
 
+// Initialize the DHT11 sensor on DHTPIN 6
 DHT dht(DHTPIN, DHTTYPE);
-
-// Stepper Motor
-const int stepsPerRevolution = 200;
 
 // Initialize the stepper library on pins 2 through 5:
 const float STEPS_PER_REV = 32;
-const float GEAR_RED = 64;
-const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
-int StepsRequired;
-int Pval;
-int potVal;
 Stepper myStepper(STEPS_PER_REV, 2, 4, 3, 5);
+
+// Initialize the LCD
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+// Initialize a non-blocking delay
+unsigned long int delayStart = millis();
+
+// Define Port A Register Pointers
+volatile unsigned char *port_a = (unsigned char *)0x22;
+volatile unsigned char *ddr_a = (unsigned char *)0x21;
+volatile unsigned char *pin_a = (unsigned char *)0x20;
 
 // Define Port B Register Pointers
 volatile unsigned char *port_b = (unsigned char *)0x25;
@@ -43,162 +48,180 @@ volatile unsigned char *port_d = (unsigned char *)0x2B;
 volatile unsigned char *ddr_d = (unsigned char *)0x2A;
 volatile unsigned char *pin_d = (unsigned char *)0x29;
 
+// Define UART Pointers
 volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
 volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
 volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
 volatile unsigned int *myUBRR0 = (unsigned int *)0x00C4;
 volatile unsigned char *myUDR0 = (unsigned char *)0x00C6;
 
+// Define ADC Pointers
 volatile unsigned char *my_ADMUX = (unsigned char *)0x7C;
 volatile unsigned char *my_ADCSRB = (unsigned char *)0x7B;
 volatile unsigned char *my_ADCSRA = (unsigned char *)0x7A;
 volatile unsigned int *my_ADC_DATA = (unsigned int *)0x78;
 
+// Macros
 #define WRITE_HIGH_PD(pin_num) *port_d |= (0x01 << pin_num);
 #define WRITE_LOW_PD(pin_num) *port_d &= ~(0x01 << pin_num);
-// LCD
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+#define WRITE_HIGH_PA(pin_num) *port_a |= (0x01 << pin_num);
+#define WRITE_LOW_PA(pin_num) *port_a &= ~(0x01 << pin_num);
+
+// Constants
+bool errMessage = false;
+bool canRun = false;
+//DS1307 clock;//define a object of DS1307 class
 
 void setup() {
-  // setup the UART
-  //U0init(9600);
-  Serial.begin(9600);
+//  *ddr_a |= 0x01 << 0b00000001;  // Output Port A23 (Pin 1) [Yellow] - Disabled / Fan Off Running
+//  *ddr_a |= 0x01 << 0b00000011;  // Output Port A25 (Pin 3) [Blue] - Fan Running
+//  *ddr_a |= 0x01 << 0b00000101;  // Output Port A27 (Pin 5) [Red] - Error
+//  *ddr_a |= 0x01 << 0b00000111;  // Output Port A29 (Pin 7) [Green] - Fan Off Running
+
+  U0init(9600); // setup the UART
+
+  // External Modules : DHT11
   dht.begin();
+
+  // External Modules : LCD
   lcd.begin(16, 2);
-  myStepper.setSpeed(200);
   lcd.clear();
+
+  // External Modules : Stepper
+  myStepper.setSpeed(200);
+
+  // External Modules : ADC
+  adc_init();
+
+  // External Modules : RTC
+//  clock.begin();
+//  clock.fillByYMD(2022,11,23);//Nov 23,2022
+//  clock.fillByHMS(9,01,00);//09:01 00
+//  clock.fillDayOfWeek(WED);//
+//  clock.setTime();//write time to the RTC chip
   
-  /*
-  //RTC setup
-  clock.begin();
-  clock.fillByYMD(2022,11,23);//Nov 23,2022
-  clock.fillByHMS(09,01,00);//09:01 00
-  clock.fillDayOfWeek(WED);//
-  clock.setTime();//write time to the RTC chip
-  */
-  
-  //lcd.print("Water Level: ");
-
-  //adc_init();
-
-  // PD0
-
 }
 
 void loop() {
-  lcd.clear();
+  // External Modules : Stepper Direction
   bool button_1 = *pin_d & 0b00000010;
-  bool button_2 = *pin_d & 0b00000001;   
-  if (button_1){
-    myStepper.step(50);
-    //printTime();
-
+  bool button_2 = *pin_d & 0b00000001;
+  if (!errMessage){
+    if (button_1) {
+      myStepper.step(50);
+    } else if (button_2) {
+      myStepper.step(-50);
+    }
   }
-  if (button_2){
-    myStepper.step(-50);
-    //printTime();
 
+  // External Modules : DHT / LCD Clearing
+  if (!errMessage && ((millis() - delayStart) >= 5000)) {
+    lcd.clear();
+    delayStart = millis();
+    float humidity = dht.readHumidity();
+    float temp_celsius = dht.readTemperature();
+    float temp_farenheit = dht.readTemperature(true);
+    lcd.print("Temp   : ");
+    lcd.print(temp_farenheit);
+    lcd.print("\337F");
+    lcd.setCursor(0, 1);
+    lcd.print("Humid %: ");
+    lcd.print(humidity);
   }
-  float humidity = dht.readHumidity();
-  float temp_celsius = dht.readTemperature();
-  float temp_farenheit = dht.readTemperature(true);
-  lcd.print("Temp   : ");
-  lcd.print(temp_farenheit);
-  lcd.print("\337F");
-  lcd.setCursor(0,1);
-  lcd.print("Humid %: ");
-  lcd.print(humidity);
-  delay(1000);
 
+  // External Modules : Fan Motor / Running / Stopping
+  //if (canRun & startButton || canRun & idleState){
+    
+  //}
 
-  // for (int pos = 0; pos < 13; pos++){
-  //   lcd.scrollDisplayLeft();
-  //   delay(10000);
-  // }
-  // for (int pos = 0; pos < 13; pos++){
-  //   lcd.scrollDisplayRight();
-  //   delay(10000);
-  // }
-
-
-  // get the reading from the ADC
-  // unsigned int adc_reading = adc_read(0);
-  // print_int(adc_reading);
-  // if (adc_reading > 200){
-  //   WRITE_HIGH_PD(7);
-  // } else {
-  //   WRITE_LOW_PD(7);
-  // }
+  // External Modules : ADC
+  unsigned int adc_reading = adc_read(0);
+  if (adc_reading < 250) {
+    canRun = false;
+    errMessage = true;
+    *ddr_a |= 0x01 << 0b00000101;
+    WRITE_HIGH_PA(5);
+    //printTime();
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Water Level");
+    lcd.setCursor(0, 1);
+    lcd.print("Too Low...");
+  } else {
+    canRun = true;
+    errMessage = false;
+    *ddr_a |= 0x01 << 0b00000101;
+    WRITE_LOW_PA(5);
+  }
 }
+
+
+
+
+
+// External Functions
+
+//void printTime(){
+//    clock.getTime();
+//    Serial.print(clock.hour, DEC);
+//    Serial.print(":");
+//    Serial.print(clock.minute, DEC);
+//    Serial.print(":");
+//    Serial.print(clock.second, DEC);
+//    Serial.print("  ");
+//    Serial.print(clock.month, DEC);
+//    Serial.print("/");
+//    Serial.print(clock.dayOfMonth, DEC);
+//    Serial.print("/");
+//    Serial.print(clock.year+2000, DEC);
+//    Serial.print(" ");
+//    Serial.print(clock.dayOfMonth);
+//    Serial.println(" ");
+//}
 
 void adc_init() {
-  // setup the A register
-  *my_ADCSRA |= 0b10000000;  // set bit   7 to 1 to enable the ADC
-  *my_ADCSRA &= 0b11011111;  // clear bit 6 to 0 to disable the ADC trigger mode
-  *my_ADCSRA &= 0b11110111;  // clear bit 5 to 0 to disable the ADC interrupt
-  *my_ADCSRA &= 0b11111000;  // clear bit 0-2 to 0 to set prescaler selection to slow reading
-  // setup the B register
-  *my_ADCSRB &= 0b11110111;  // clear bit 3 to 0 to reset the channel and gain bits
-  *my_ADCSRB &= 0b11111000;  // clear bit 2-0 to 0 to set free running mode
-  // setup the MUX Register
-  *my_ADMUX &= 0b01111111;  // clear bit 7 to 0 for AVCC analog reference
-  *my_ADMUX |= 0b01000000;  // set bit   6 to 1 for AVCC analog reference
-  *my_ADMUX &= 0b11011111;  // clear bit 5 to 0 for right adjust result
-  *my_ADMUX &= 0b11100000;  // clear bit 4-0 to 0 to reset the channel and gain bits
+  *my_ADCSRA |= 0b10000000;
+  *my_ADCSRA &= 0b11011111;
+  *my_ADCSRA &= 0b11110111;
+  *my_ADCSRA &= 0b11111000;
+  *my_ADCSRB &= 0b11110111;
+  *my_ADCSRB &= 0b11111000;
+  *my_ADMUX &= 0b01111111;
+  *my_ADMUX |= 0b01000000;
+  *my_ADMUX &= 0b11011111;
+  *my_ADMUX &= 0b11100000;
 }
 unsigned int adc_read(unsigned char adc_channel_num) {
-  // clear the channel selection bits (MUX 4:0)
   *my_ADMUX &= 0b11100000;
-  // clear the channel selection bits (MUX 5)
   *my_ADCSRB &= 0b11110111;
-  // set the channel number
   if (adc_channel_num > 7) {
-    // set the channel selection bits, but remove the most significant bit (bit 3)
     adc_channel_num -= 8;
-    // set MUX bit 5
     *my_ADCSRB |= 0b00001000;
   }
-  // set the channel selection bits
   *my_ADMUX += adc_channel_num;
-  // set bit 6 of ADCSRA to 1 to start a conversion
   *my_ADCSRA |= 0x40;
-  // wait for the conversion to complete
-  while ((*my_ADCSRA & 0x40) != 0)
-    ;
-  // return the result in the ADC data register
+  while ((*my_ADCSRA & 0x40) != 0);
   return *my_ADC_DATA;
 }
 
 void print_int(unsigned int out_num) {
-  // clear a flag (for printing 0's in the middle of numbers)
   unsigned char print_flag = 0;
-  // if its greater than 1000
   if (out_num >= 1000) {
-    // get the 1000's digit, add to '0'
     U0putchar(out_num / 1000 + '0');
-    // set the print flag
     print_flag = 1;
-    // mod the out num by 1000
     out_num = out_num % 1000;
   }
-  // if its greater than 100 or we've already printed the 1000's
   if (out_num >= 100 || print_flag) {
-    // get the 100's digit, add to '0'
     U0putchar(out_num / 100 + '0');
-    // set the print flag
     print_flag = 1;
-    // mod the output num by 100
     out_num = out_num % 100;
   }
-  // if its greater than 10, or we've already printed the 10's
   if (out_num >= 10 || print_flag) {
     U0putchar(out_num / 10 + '0');
     print_flag = 1;
     out_num = out_num % 10;
   }
-  // always print the last digit (in case it's 0)
   U0putchar(out_num + '0');
-  // print a newline
   U0putchar('\n');
 }
 
@@ -206,33 +229,12 @@ void U0init(int U0baud) {
   unsigned long FCPU = 16000000;
   unsigned int tbaud;
   tbaud = (FCPU / 16 / U0baud - 1);
-  // Same as (FCPU / (16 * U0baud)) - 1;
   *myUCSR0A = 0x20;
   *myUCSR0B = 0x18;
   *myUCSR0C = 0x06;
   *myUBRR0 = tbaud;
+  Serial.begin(9600);
 }
-
-/*
-void printTime(){
-
-    clock.getTime();
-    Serial.print(clock.hour, DEC);
-    Serial.print(":");
-    Serial.print(clock.minute, DEC);
-    Serial.print(":");
-    Serial.print(clock.second, DEC);
-    Serial.print("  ");
-    Serial.print(clock.month, DEC);
-    Serial.print("/");
-    Serial.print(clock.dayOfMonth, DEC);
-    Serial.print("/");
-    Serial.print(clock.year+2000, DEC);
-    Serial.print(" ");
-    Serial.print(clock.dayOfMonth);
-    Serial.println(" ");
-}
-*/
 unsigned char U0kbhit() {
   return *myUCSR0A & RDA;
 }
@@ -240,7 +242,6 @@ unsigned char U0getchar() {
   return *myUDR0;
 }
 void U0putchar(unsigned char U0pdata) {
-  while ((*myUCSR0A & TBE) == 0)
-    ;
+  while ((*myUCSR0A & TBE) == 0);
   *myUDR0 = U0pdata;
 }
